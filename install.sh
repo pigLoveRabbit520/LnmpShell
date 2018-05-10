964c41adeb65566fecd800ea38783ff146bae282
salamander-mh <1906747819@qq.com>
2018-05-10 17:06:40 +0800

    安装nginx和php可以选择安装

--- install.sh
+++ install.sh
@@ -44,132 +44,130 @@ Get_Dist_Name()
     echo "Your Linux Distribution is ${DISTRO}";
 }
 
-# Check if user is root
-if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install lnmp"
-    exit 1
-fi
+install_dependencies()
+{
+    if [[ $DISTRO == 'CentOS' || $DISTRO == 'RHEL' || $DISTRO == 'Fedora' ]]; then
+        yum install -y gcc gcc-c++
+        yum install -y libxml2 libxml2-devel openssl openssl-devel curl-devel libjpeg-devel libpng-devel freetype-devel mysql-devel
+    elif [[ $DISTRO == 'Debian' || $DISTRO == 'Ubuntu' ]]; then
+        apt-get install -y gcc g++ make openssl pkg-config libssl-dev  libcurl4-openssl-dev \
+        libxml2 libxml2-dev libjpeg-dev libpng-dev libfreetype6-dev default-libmysqlclient-dev
+    fi
+}
 
-Get_Dist_Name
-
-if [[ $DISTRO == 'CentOS' || $DISTRO == 'RHEL' || $DISTRO == 'Fedora' ]]; then
-    yum install -y gcc gcc-c++
-    yum install -y libxml2 libxml2-devel openssl openssl-devel curl-devel libjpeg-devel libpng-devel freetype-devel mysql-devel
-elif [[ $DISTRO == 'Debian' || $DISTRO == 'Ubuntu' ]]; then
-    apt-get install -y gcc g++ make openssl pkg-config libssl-dev  libcurl4-openssl-dev \
-    libxml2 libxml2-dev libjpeg-dev libpng-dev libfreetype6-dev default-libmysqlclient-dev
-fi
-
-
-
-# 1. nginx安装
-wget http://nginx.org/download/nginx-${nginx_version}.tar.gz
-wget https://svwh.dl.sourceforge.net/project/pcre/pcre/${pcre_version}/pcre-${pcre_version}.tar.gz
-wget https://zlib.net/zlib-${zlib_version}.tar.gz
-tar -zxvf nginx-${nginx_version}.tar.gz 
-tar -zxf pcre-${pcre_version}.tar.gz
-tar -zxf zlib-${zlib_version}.tar.gz
+install_nginx() 
+{
+    cd $basepath
+    # 1. nginx安装
+    wget http://nginx.org/download/nginx-${nginx_version}.tar.gz
+    wget https://svwh.dl.sourceforge.net/project/pcre/pcre/${pcre_version}/pcre-${pcre_version}.tar.gz
+    wget https://zlib.net/zlib-${zlib_version}.tar.gz
+    tar -zxvf nginx-${nginx_version}.tar.gz 
+    tar -zxf pcre-${pcre_version}.tar.gz
+    tar -zxf zlib-${zlib_version}.tar.gz
 
 
-# nginx安装 注意 --with-pcre=  --with-zlib --with-openssl  指的是源码路径
-cd ./nginx-${nginx_version}
-./configure --prefix=/usr/local/nginx-${nginx_version} --with-pcre=./../pcre-${pcre_version}  --with-zlib=./../zlib-${zlib_version} --with-http_stub_status_module \
---with-http_ssl_module
-make
-make install
+    # nginx安装 注意 --with-pcre=  --with-zlib --with-openssl  指的是源码路径
+    cd ./nginx-${nginx_version}
+    ./configure --prefix=/usr/local/nginx-${nginx_version} --with-pcre=./../pcre-${pcre_version}  --with-zlib=./../zlib-${zlib_version} --with-http_stub_status_module \
+    --with-http_ssl_module
+    make
+    make install
 
 
-echo 'Nginx installed successfully!'
+    echo 'Nginx installed successfully!'
+}
 
+install_php()
+{
+    # 2. php安装 php 7.1以下支持mcrypt
+    cd $basepath
 
-# 2. php安装 php 7.1以下支持mcrypt
-cd $basepath
+    # 安装libmcrypt库
+    wget https://cfhcable.dl.sourceforge.net/project/mcrypt/Libmcrypt/${libmcrypt_version}/libmcrypt-${libmcrypt_version}.tar.gz
+    tar zxvf libmcrypt-${libmcrypt_version}.tar.gz
+    cd libmcrypt-${libmcrypt_version}
+    ./configure
+    make
+    make install
 
-# 安装libmcrypt库
-wget https://cfhcable.dl.sourceforge.net/project/mcrypt/Libmcrypt/${libmcrypt_version}/libmcrypt-${libmcrypt_version}.tar.gz
-tar zxvf libmcrypt-${libmcrypt_version}.tar.gz
-cd libmcrypt-${libmcrypt_version}
-./configure
-make
-make install
+    cd $basepath
 
-cd $basepath
+    # 安装mhash库
+    wget https://versaweb.dl.sourceforge.net/project/mhash/mhash/${mhash_version}/mhash-${mhash_version}.tar.gz
+    tar zxvf mhash-${mhash_version}.tar.gz
+    cd mhash-${mhash_version}
+    ./configure
+    make
+    make install
 
-# 安装mhash库
-wget https://versaweb.dl.sourceforge.net/project/mhash/mhash/${mhash_version}/mhash-${mhash_version}.tar.gz
-tar zxvf mhash-${mhash_version}.tar.gz
-cd mhash-${mhash_version}
-./configure
-make
-make install
+    cd $basepath
 
-cd $basepath
-
-# 安装mcrypt库
-wget https://astuteinternet.dl.sourceforge.net/project/mcrypt/MCrypt/${mcrypt_version}/mcrypt-${mcrypt_version}.tar.gz
-tar -zxvf mcrypt-${mcrypt_version}.tar.gz
-cd mcrypt-${mcrypt_version}
-export LD_LIBRARY_PATH=/usr/local/lib
-./configure
-make
-make install
+    # 安装mcrypt库
+    wget https://astuteinternet.dl.sourceforge.net/project/mcrypt/MCrypt/${mcrypt_version}/mcrypt-${mcrypt_version}.tar.gz
+    tar -zxvf mcrypt-${mcrypt_version}.tar.gz
+    cd mcrypt-${mcrypt_version}
+    export LD_LIBRARY_PATH=/usr/local/lib
+    ./configure
+    make
+    make install
 
 
 
-cd $basepath
+    cd $basepath
 
-tar -zxvf php-${php_version}.tar.gz
-cd ./php-${php_version}
+    tar -zxvf php-${php_version}.tar.gz
+    cd ./php-${php_version}
 
 
-./configure \
---prefix=/usr/local/php7 \
---with-config-file-path=/usr/local/php7/etc \
---enable-fpm \
---with-fpm-user=www \
---with-fpm-group=www \
---with-mysqli \
---with-pdo-mysql \
---with-libdir=lib64 \
---with-iconv-dir \
---with-freetype-dir \
---with-jpeg-dir \
---with-png-dir \
---with-zlib \
---with-libxml-dir=/usr \
---enable-xml \
---disable-rpath  \
---enable-bcmath \
---enable-shmop \
---enable-sysvsem \
---enable-inline-optimization \
---with-curl \
---enable-mbregex \
---enable-mbstring \
---with-mcrypt \
---enable-ftp \
---with-gd \
---enable-gd-native-ttf \
---with-openssl \
---with-mhash \
---enable-pcntl \
---enable-sockets \
---with-xmlrpc \
---enable-zip \
---enable-soap \
---without-pear \
---with-gettext \
---disable-fileinfo \
---enable-maintainer-zts
+    ./configure \
+    --prefix=/usr/local/php7 \
+    --with-config-file-path=/usr/local/php7/etc \
+    --enable-fpm \
+    --with-fpm-user=www \
+    --with-fpm-group=www \
+    --with-mysqli \
+    --with-pdo-mysql \
+    --with-libdir=lib64 \
+    --with-iconv-dir \
+    --with-freetype-dir \
+    --with-jpeg-dir \
+    --with-png-dir \
+    --with-zlib \
+    --with-libxml-dir=/usr \
+    --enable-xml \
+    --disable-rpath  \
+    --enable-bcmath \
+    --enable-shmop \
+    --enable-sysvsem \
+    --enable-inline-optimization \
+    --with-curl \
+    --enable-mbregex \
+    --enable-mbstring \
+    --with-mcrypt \
+    --enable-ftp \
+    --with-gd \
+    --enable-gd-native-ttf \
+    --with-openssl \
+    --with-mhash \
+    --enable-pcntl \
+    --enable-sockets \
+    --with-xmlrpc \
+    --enable-zip \
+    --enable-soap \
+    --without-pear \
+    --with-gettext \
+    --disable-fileinfo \
+    --enable-maintainer-zts
 
-make 
-make install
+    make 
+    make install
 
-# create a link to php
-ln -s /usr/local/php7/bin/php /usr/local/bin/
+    # create a link to php
+    ln -s /usr/local/php7/bin/php /usr/local/bin/
 
-# write php-fpm configure
-cat > /usr/local/php7/etc/php-fpm.conf <<EOF
+    # write php-fpm configure
+    cat > /usr/local/php7/etc/php-fpm.conf <<EOF
 [global]
 pid = /usr/local/php/var/run/php-fpm.pid
 error_log = /usr/local/php/var/log/php-fpm.log
@@ -192,7 +190,32 @@ pm.max_spare_servers = 60
 request_terminate_timeout = 100
 request_slowlog_timeout = 0
 slowlog = var/log/slow.log
-
 EOF
 
-echo 'PHP installed successfully!'
+    echo 'PHP installed successfully!'
+}
+
+##### 程序正文
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install lnmp"
+    exit 1
+fi
+
+Get_Dist_Name
+echo -e "Which do you want to install?\n1. nginx\n2. php\n3. nginx and php"
+read choose
+if [[ $choose = '1' ]]; then
+    install_dependencies
+    install_nginx
+elif [[ $choose = '2' ]]; then
+    install_dependencies
+    install_php
+elif [[ $choose = '3' ]]; then
+    install_dependencies
+    install_nginx
+    install_php
+else
+    echo "Nothing to install."
+fi
