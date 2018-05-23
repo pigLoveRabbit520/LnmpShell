f812ecb0811cf8351fcdaa4d6d26458d6709ad9d
salamander-mh <1906747819@qq.com>
2018-05-23 17:04:22 +0800

    编译PHP开启sockets扩展

--- install.sh
+++ install.sh
@@ -138,6 +138,7 @@ install_php()
             --with-zlib \
             --with-libxml-dir=/usr \
             --enable-xml \
+            --enable-sockets \
             --disable-rpath  \
             --enable-bcmath \
             --enable-shmop \
